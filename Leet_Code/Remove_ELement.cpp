e56f991f7769229ec0ab1db33ffaa5e3e39456ac
HolyNekK <e238109@metu.edu.tr>
2020-10-27 14:51:31 +0300

    sol

---
+++ Leet_Code/Remove_ELement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size(), i = 0;
+        while(i < n){
+            if (nums[i] == val){
+                nums.erase(nums.begin()+i);
+                n--;
+                continue;
+            }i++;
+        }return n;
+    }
+};
