bd36a25d4e426ff187f06cd6344222c066373570
HolyNekK <e238109@metu.edu.tr>
2020-11-18 10:48:01 +0300

    Solution

---
+++ Leet_Code/Intersection_of_Two_LinkedList.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode * p1 = headA, * p2 = headB;
+        while(p2 != p1){
+            p1 = p1 ? p1->next : headA;
+            p2 = p2 ? p2->next : headB;
+        }return p2;
+    }
+};
