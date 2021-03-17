6307dceb06b95e8d3002dc934f7f23f7afd19fbc
HolynekK <e238109@metu.edu.tr>
2021-03-18 01:09:00 +0300

    solution added

---
+++ Leet_Code/Count_of_Matches_in_Tournament.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int matches = 0;
+        while(n != 1){
+            matches += n/2;
+            if(n % 2){
+                n = n/2+1;
+            }else{
+                n = n/2;
+            }
+        }
+        return matches;
+    }
+};
