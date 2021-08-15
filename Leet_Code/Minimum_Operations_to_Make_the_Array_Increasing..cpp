5fef695bfd298967b893cda20d79863627a09d17
HolyNekK <e238109@metu.edu.tr>
2021-08-15 21:46:59 +0300

    solution added

---
+++ Leet_Code/Minimum_Operations_to_Make_the_Array_Increasing..cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans= 0;
+        for(int i = 1; i < size(nums); i++){
+            if (nums[i] <= nums[i-1]){
+                ans += nums[i-1] - nums[i] + 1;
+                nums[i] += nums[i-1] - nums[i] + 1;
+            }else;
+        }
+        return ans;
+    }
+};
