372bf522ad4558684ef8eab4da14807b21d2d42e
holynekk <e238109@metu.edu.tr>
2022-09-05 21:44:38 +0300

    solution added

---
+++ Leet_Code/Range_Sum_Query-Immutable.py
@@ -0,0 +1,16 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.arr = [nums[0]]
+        for i in range(1, len(nums)):
+            self.arr.append(self.arr[i-1]+nums[i])
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.arr[right]
+        return self.arr[right] - self.arr[left-1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
