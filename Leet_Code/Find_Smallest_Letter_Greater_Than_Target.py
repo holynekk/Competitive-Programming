061a02d39ff9f802184625b0791db9435d268e7a
holynekk <e238109@metu.edu.tr>
2022-09-05 22:11:43 +0300

    solution added

---
+++ Leet_Code/Find_Smallest_Letter_Greater_Than_Target.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters)
+        mid = None
+        
+        while left < right:
+            mid = (left+right)//2
+            if ord(letters[mid]) <= ord(target):
+                left = mid + 1
+            else:
+                right = mid
+        return letters[left % len(letters)]
